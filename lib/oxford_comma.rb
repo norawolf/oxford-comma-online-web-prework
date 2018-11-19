100052d7f955ac8dbba8f9dd23f3592992b39867
Nora Vogt <noraevogt@gmail.com>
2018-11-19 19:09:52 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -13,17 +13,15 @@ end
 
 # original attempt below.
 
-# def oxford_comma(array)
+ def oxford_comma(array)
 
-#new_array = []
-
-#  if array.length == 1
-#    array.first
-#  elsif array.length == 2
-#    array.join(" and ")
-#  elsif array.length == 3
-#      "#{array[0]}, #{array[1]}, and #{array[2]}"
-#  elsif array.length > 3
-#    array[0...-1].join(", ") << ", and #{array.pop}"
-#  end
-#end
+  if array.length == 1
+    array.first
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length == 3
+      "#{array[0]}, #{array[1]}, and #{array[2]}"
+  elsif array.length > 3
+    array[0...-1].join(", ") << ", and #{array.pop}"
+  end
+end

b2da7445d458377c7f0e24070a79428e57993ccc
Nora Vogt <noraevogt@gmail.com>
2018-11-19 19:06:22 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,14 +1,28 @@
+# wanted to try again using a case statement
 def oxford_comma(array)
-
-new_array = []
-
-  if array.length == 1
+  case array.length
+  when 1
     array.first
-  elsif array.length == 2
+  when 2
     array.join(" and ")
-  elsif array.length == 3
-      "#{array[0]}, #{array[1]}, and #{array[2]}"
-  elsif array.length > 3
+  when > 2
     array[0...-1].join(", ") << ", and #{array.pop}"
-  end
 end
+
+
+# original attempt below. 
+
+# def oxford_comma(array)
+
+#new_array = []
+
+#  if array.length == 1
+#    array.first
+#  elsif array.length == 2
+#    array.join(" and ")
+#  elsif array.length == 3
+#      "#{array[0]}, #{array[1]}, and #{array[2]}"
+#  elsif array.length > 3
+#    array[0...-1].join(", ") << ", and #{array.pop}"
+#  end
+#end

1dfe881f4a36f3260c1dbe009ecfdcc82836c672
Nora Vogt <noraevogt@gmail.com>
2018-11-19 18:58:38 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
 
+new_array = []
+
+  if array.length == 1
+    array.first
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length == 3
+      "#{array[0]}, #{array[1]}, and #{array[2]}"
+  elsif array.length > 3
+    array[0...-1].join(", ") << ", and " << array.pop
+  end
 end

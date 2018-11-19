fdab6d112b467f4cba0b3551e69d16fa8bcc0e0c
Nora Vogt <noraevogt@gmail.com>
2018-11-19 19:03:12 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -9,6 +9,6 @@ new_array = []
   elsif array.length == 3
       "#{array[0]}, #{array[1]}, and #{array[2]}"
   elsif array.length > 3
-    array[0...-1].join(", ") << ", and " << array.pop
+    array[0...-1].join(", ") << ", and #{array.pop}"
   end
 end
